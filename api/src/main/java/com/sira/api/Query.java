d3d72a790676857348773296e48623379bb6ae29
Yared.negede <yared.negede@dventus.com>
2018-01-21 20:54:33 +0300

    Skill controller added

--- api/src/main/java/com/sira/api/Query.java
+++ api/src/main/java/com/sira/api/Query.java
@@ -2,5 +2,48 @@ package com.sira.api;
 
 public abstract class Query {
 
+	private String allEmployee = "from Employee";	
+	private String allEmployer = "from Employer";	
+	private String allProject = "from Project";
+	private String allSkill = "from Skill";
+	
+	private String employeeProject = "from Project as p where p.name=";	
+	private String employeeSkill = "from Skill as s where s.name=";
+	private String employeeProfile = "from Profile as p where p.name=";
+	
+	private String employerProject = "from Project as p where p.name=";	
+	private String employerSkill = "from Skill as s where s.name=";
+	private String employerProfile = "from Profile as p where p.name=";
+	
+	public String getAllEmployee() {
+		return allEmployee;
+	}
+	public String getAllEmployer() {
+		return allEmployer;
+	}
+	public String getAllProject() {
+		return allProject;
+	}
+	public String getAllSkill() {
+		return allSkill;
+	}
+	public String getEmployeeProject() {
+		return employeeProject;
+	}
+	public String getEmployeeSkill() {
+		return employeeSkill;
+	}
+	public String getEmployeeProfile() {
+		return employeeProfile;
+	}
+	public String getEmployerProject() {
+		return employerProject;
+	}
+	public String getEmployerSkill() {
+		return employerSkill;
+	}
+	public String getEmployerProfile() {
+		return employerProfile;
+	}
 	
 }
--- gateway/src/test/java/com/server/EmployeTest.java
+++ gateway/src/test/java/com/server/EmployeTest.java
@@ -59,5 +59,6 @@ public class EmployeTest {
 		server.add(resourceRequest);
 		
 	}
+	
 
 }
